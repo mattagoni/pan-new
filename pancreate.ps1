87d8bc0764d85233e8766d3a8414c3cb2d8eb05c
mattagoni <46663698+mattagoni@users.noreply.github.com>
2025-09-10 10:11:54 +1000

    Update pancreate.ps1
    update rg to new magoni

--- pancreate.ps1
+++ pancreate.ps1
@@ -1,49 +1,49 @@
 ##Deploy PaloFirewall into Hub vNET
-New-AzureRmResourceGroup -Name pan-rg -Location AustraliaSouthEast
-New-AzResourceGroupDeployment -ResourceGroupName "pan-rg" -TemplateFile "/home/matthew/pan/pandeploytemplate.json" -TemplateParameterFile "/home/matthew/pan/pandeploymentparameters.json"
+New-AzureRmResourceGroup -Name magoni -Location AustraliaSouthEast
+New-AzResourceGroupDeployment -ResourceGroupName "magoni" -TemplateFile "/home/matthew/pan/pandeploytemplate.json" -TemplateParameterFile "/home/matthew/pan/pandeploymentparameters.json"
 
 
 
 ##get Palto network ready after template deployment
-$nic = Get-AzNetworkInterface -Name paloaltofw-pan-ip-eth0 -ResourceGroup pan-rg
+$nic = Get-AzNetworkInterface -Name paloaltofw-pan-ip-eth0 -ResourceGroup magoni
 $nic.IpConfigurations.publicipaddress.id = $null
 Set-AzNetworkInterface -NetworkInterface $nic
-Remove-AzureRmPublicIpAddress -Name pan-ip -ResourceGroupName pan-rg
+Remove-AzureRmPublicIpAddress -Name pan-ip -ResourceGroupName magoni
 
-New-AzPublicIpAddress -Name Pan-mgt-Ip -ResourceGroupName Pan-rg -Location 'AustraliaSouthEast' -AllocationMethod static -sku standard
-New-AzPublicIpAddress -Name Pan-untrust-Ip -ResourceGroupName Pan-rg -Location 'AustraliaSouthEast' -AllocationMethod static -sku standard
+New-AzPublicIpAddress -Name Pan-mgt-Ip -ResourceGroupName magoni -Location 'AustraliaSouthEast' -AllocationMethod static -sku standard
+New-AzPublicIpAddress -Name Pan-untrust-Ip -ResourceGroupName magoni -Location 'AustraliaSouthEast' -AllocationMethod static -sku standard
 
 $vnet = Get-AzVirtualNetwork -Name HubNetwork -ResourceGroupName HubNetwork-RG
 $subnet = Get-AzVirtualNetworkSubnetConfig -Name Mgt -VirtualNetwork $vnet
-$nic = Get-AzNetworkInterface -Name paloaltofw-pan-ip-eth0 -ResourceGroupName pan-rg
-$pip = Get-AzPublicIpAddress -Name Pan-mgt-Ip -ResourceGroupName Pan-rg
+$nic = Get-AzNetworkInterface -Name paloaltofw-pan-ip-eth0 -ResourceGroupName magoni
+$pip = Get-AzPublicIpAddress -Name Pan-mgt-Ip -ResourceGroupName magoni
 $nic | Set-AzNetworkInterfaceIpConfig -Name ipconfig-mgmt -PublicIPAddress $pip -Subnet $subnet
 $nic | Set-AzNetworkInterface
 
 $vnet = Get-AzVirtualNetwork -Name HubNetwork -ResourceGroupName HubNetwork-RG
 $subnet = Get-AzVirtualNetworkSubnetConfig -Name Untrust -VirtualNetwork $vnet
-$nic = Get-AzNetworkInterface -Name paloaltofw-pan-ip-eth1 -ResourceGroupName pan-rg
-$pip = Get-AzPublicIpAddress -Name Pan-untrust-Ip -ResourceGroupName Pan-rg
+$nic = Get-AzNetworkInterface -Name paloaltofw-pan-ip-eth1 -ResourceGroupName magoni
+$pip = Get-AzPublicIpAddress -Name Pan-untrust-Ip -ResourceGroupName magoni
 $nic | Set-AzNetworkInterfaceIpConfig -Name ipconfig-untrust -PublicIPAddress $pip -Subnet $subnet
 $nic | Set-AzNetworkInterface
 
 $rule1 = New-AzNetworkSecurityRuleConfig -Name ALLIN -Description "Allow ALL IN" -Access Allow -Protocol * -Direction Inbound -Priority 100 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange *
 $rule2 = New-AzNetworkSecurityRuleConfig -Name ALLOUT -Description "Allow ALL OUT" -Access Allow -Protocol * -Direction Outbound -Priority 100 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange *
-$nsg = New-AzNetworkSecurityGroup -ResourceGroupName pan-rg -Location australiasoutheast -Name "AllowALL" -SecurityRules $rule1,$rule2
+$nsg = New-AzNetworkSecurityGroup -ResourceGroupName magoni -Location australiasoutheast -Name "AllowALL" -SecurityRules $rule1,$rule2
 
 
-$nic = Get-AzNetworkInterface -ResourceGroupName "pan-rg" -Name "paloaltofw-pan-ip-eth0"
-$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "pan-rg" -Name "AllowAll"
+$nic = Get-AzNetworkInterface -ResourceGroupName "magoni" -Name "paloaltofw-pan-ip-eth0"
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "magoni" -Name "AllowAll"
 $nic.NetworkSecurityGroup = $nsg
 $nic | Set-AzNetworkInterface
 
-$nic = Get-AzNetworkInterface -ResourceGroupName "pan-rg" -Name "paloaltofw-pan-ip-eth1"
-$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "pan-rg" -Name "AllowAll"
+$nic = Get-AzNetworkInterface -ResourceGroupName "magoni" -Name "paloaltofw-pan-ip-eth1"
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "magoni" -Name "AllowAll"
 $nic.NetworkSecurityGroup = $nsg
 $nic | Set-AzNetworkInterface
 
-$nic = Get-AzNetworkInterface -ResourceGroupName "pan-rg" -Name "paloaltofw-pan-ip-eth2"
-$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "pan-rg" -Name "AllowAll"
+$nic = Get-AzNetworkInterface -ResourceGroupName "magoni" -Name "paloaltofw-pan-ip-eth2"
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "magoni" -Name "AllowAll"
 $nic.NetworkSecurityGroup = $nsg
 $nic | Set-AzNetworkInterface
 
